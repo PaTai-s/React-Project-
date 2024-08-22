cbcf88c7683e25f0914228dc2bc3f8c11303912f
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-08-22 12:46:34 -0400

    website rendering after merge

--- Patai-cookbook/src/main.jsx
+++ Patai-cookbook/src/main.jsx
@@ -2,22 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import MealProvider from './Context /MealProvider.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-
-import MealProvider from './Context /MealProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <MealProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </MealProvider>
 
 )
+
