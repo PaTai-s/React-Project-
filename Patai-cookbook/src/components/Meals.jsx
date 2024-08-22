6909af363f8fe0b59513e5c52d9857f42d79fe9f
Taifinkaba <taifinkabad@gmail.com>
2024-08-22 12:36:15 -0400

    maid home.jsx, app.jsx, i  made a  navbar, took 4 hrs

---
+++ Patai-cookbook/src/components/Meals.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Meals = () => {
+    return <h1>Meals</h1>
+}
+
+
+export default Meals;
---
+++ Patai-cookbook/src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    // search bar with useState
+    const [searchTerm, setSearchTerm] = useState('');
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // handle search logic here
+        console.log(`Searching for ${searchTerm}...`);
+    }
+    // search button code above
+
+
+    return (
+    <nav className="nav">
+        <Link to="/" className="site-title">
+            PaTais CookBook
+            </Link>
+        <ul>
+            <Link to='/home'>Home</Link>
+            <Link to='/meals'>Meals</Link>
+        </ul>
+        
+        {/* search button */}
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={searchTerm} onChange={handleChange} />
+            <button type="submit">Search</button>
+            </form>
+    </nav>
+    )
+  };
+
+export default Navbar;
