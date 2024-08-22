c03bca9f526297c8104fc61a7703985f4baf150c
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-08-22 12:28:20 -0400

    MealBySearch fetch and functionality complete

--- Patai-cookbook/src/App.jsx
+++ Patai-cookbook/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import MealBySearch from './components/MealBySearch';
 
 
+
 function App() {
 
 
--- Patai-cookbook/src/components/MealBySearch.jsx
+++ Patai-cookbook/src/components/MealBySearch.jsx
@@ -1,34 +1,75 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import fetchData from "../utils/fetchData";
 import React from "react";
+import MealContext from "../Context /MealContext";
 
 const MealBySearch = () => {
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null)
+    const [query, setSearch] = useState('');
+    const [error, setError] = useState('')
 
-    useEffect(() => {
-        const doFetch = async () => {
-            const response = await fetchData(`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`)
-            // const response = fetchData.json()
-            if (response) setData(response[0]['meals'])
-            if (error) setError(error.message)
-        }
-        doFetch()
-    }, [])
+    const { setMeals } = useContext(MealContext)
 
-    useEffect(() => {
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const [data, error] = await fetchData(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+        if (data) setMeals(data)
         console.log(data)
-    }, [data])
 
+        if (error) setError(error.message);
+
+    }
+    if (error) return <p>{error.message}</p>
 
 
     return (
-        <>
+        <form>
+            <label htmlFor="MealSearch">Search for a recipe </label>
+            <input type="text" value={query} onChange={(e) => setSearch(e.target.value)} />
+            <button onClick={handleSubmit} type="submit">Search</button>
 
-
-        </>
+        </form>
     )
 }
 
+
+// function GifSearch({ setData }) {
+
+//     const [query, setSearch] = useState('')
+//     const [error, setError] = useState('')
+
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault()
+
+//         const [data, error] = await fetchData(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=3&rating=g`);
+//         if (data) setData(data);
+//         if (error) setError(error.message);
+//     }
+//     if (error) return <p>{error.message}</p>
+//     console.log(query)
+//     return (
+//         <form>
+//             <label htmlFor="searchInput">Enter a Search Term </label>
+//             <input type="text" value={query} onChange={(e) => setSearch(e.target.value)} className="form-control" id="searchInput" />
+//             <button onClick={handleSubmit} type="submit" className="btn btn-success">Search</button>
+//         </form>
+//     )
+// }
+
+
+// useEffect(() => {
+//     const doFetch = async () => {
+//         const response = await fetchData(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
+//         // const response = fetchData.json()
+//         if (response) setData(response[0]['meals'])
+//         if (error) setError(error.message)
+//     }
+//     doFetch()
+// }, [])
+
+// useEffect(() => {
+// console.log(data)
+// }, [data])
 
 export default MealBySearch
